ae280f06f3910522e1fd7e95ec1254162a8e3835
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-09-03 15:21:10 -0300

    Coordenadas de um Ponto

---
+++ beecrowd1041.js
@@ -0,0 +1,45 @@
+/*
+    Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto 
+    em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um 
+    dos eixos cartesianos ou na origem (x = y = 0).
+                    X
+                    Q2 | Q1
+                    ------- Y
+                    Q3 | Q4
+
+    Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+    Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+    Entrada:
+    A entrada contem as coordenadas de um ponto.
+
+    Saída:
+    A saída deve apresentar o quadrante em que o ponto se encontra.
+*/
+
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var informacoesDaLinha1 = lines.shift().split(' ');
+
+let x = parseFloat(informacoesDaLinha1.shift());
+let y = parseFloat(informacoesDaLinha1.shift());
+
+if (x !== 0 && y !== 0) {
+    if (x > 0 && y > 0) {
+        console.log('Q1');
+    } else if (x < 0 && y > 0) {
+        console.log('Q2');
+    } else if (x < 0 && y < 0) {
+        console.log('Q3');
+    } else {
+        console.log('Q4');
+    }
+} else if (x !== 0 && y === 0) {
+    console.log('Eixo X');
+} else if (y !== 0 && x === 0) {
+    console.log('Eixo Y');
+} else {
+    console.log('Origem');
+}
