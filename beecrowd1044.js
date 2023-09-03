f729eba99538c568c356ad759709d1c037710199
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-09-03 18:05:44 -0300

    Múltiplos

---
+++ beecrowd1044.js
@@ -0,0 +1,24 @@
+/*
+    Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", 
+    indicando se os valores lidos são múltiplos entre si.
+
+    Entrada:
+    A entrada contém valores inteiros.
+
+    Saída:
+    A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var informacoesDaLinha1 = lines.shift().split(' ');
+
+let A = parseInt(informacoesDaLinha1.shift());
+let B = parseInt(informacoesDaLinha1.shift());
+
+if (A % B === 0 || B % A === 0) {
+    console.log('Sao Multiplos');
+} else {
+    console.log('Nao sao Multiplos');
+}
